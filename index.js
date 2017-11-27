06454261015b1dd11fa0d545b0222b4af56dd5cd
Christan <christanfoden@me.com>
2017-11-27 11:08:09 +0000

    notifications-working

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 import React, { Component } from 'react';
-import { AppRegistry } from 'react-native';
+import { AppRegistry, Platform } from 'react-native';
 import firebase from 'firebase';
-import {Container, StyleProvider,  Header, Left, Right, Icon, Button, Body} from 'native-base';
+import { Container, StyleProvider,  Header, Left, Right, Icon, Button, Body } from 'native-base';
+import FCM, {FCMEvent, RemoteNotificationResult, WillPresentNotificationResult, NotificationType} from 'react-native-fcm';
+import { Router, Scene } from 'react-native-router-flux';
 
 // import Themes
 import getTheme from './src/themes/components';
@@ -18,7 +20,41 @@ import News from './src/components/pages/News';
 import Events from './src/components/pages/Events';
 import Settings from './src/components/pages/Settings';
 
-import { Router, Scene } from 'react-native-router-flux';
+// this shall be called regardless of app state: running, background or not running. Won't be called when app is killed by user in iOS
+FCM.on(FCMEvent.Notification, async (notif) => {
+    // there are two parts of notif. notif.notification contains the notification payload, notif.data contains data payload
+    if (notif.local_notification){
+      //this is a local notification
+    }
+    if (notif.opened_from_tray){
+      //iOS: app is open/resumed because user clicked banner
+      //Android: app is open/resumed because user clicked banner or tapped app icon
+    }
+    // await someAsyncCall();
+
+    if (Platform.OS ==='ios'){
+      //optional
+      //iOS requires developers to call completionHandler to end notification process. If you do not call it your background remote notifications could be throttled, to read more about it see the above documentation link.
+      //This library handles it for you automatically with default behavior (for remote notification, finish with NoData; for WillPresent, finish depend on "show_in_foreground"). However if you want to return different result, follow the following code to override
+      //notif._notificationType is available for iOS platfrom
+      switch (notif._notificationType){
+        case NotificationType.Remote:
+          notif.finish(RemoteNotificationResult.NewData) //other types available: RemoteNotificationResult.NewData, RemoteNotificationResult.ResultFailed
+          break;
+        case NotificationType.NotificationResponse:
+          notif.finish();
+          break;
+        case NotificationType.WillPresent:
+          notif.finish(WillPresentNotificationResult.All) //other types available: WillPresentNotificationResult.None
+          break;
+      }
+    }
+});
+
+FCM.on(FCMEvent.RefreshToken, (token) => {
+    console.log(token)
+    // fcm token may not be available on first load, catch it here
+});
 
 export default class newsApp extends Component {
   state = { loggedIn: null };
@@ -31,7 +67,7 @@ export default class newsApp extends Component {
     projectId: 'news-8436e',
     storageBucket: 'news-8436e.appspot.com',
     messagingSenderId: '863933432148'
-    });
+  });
 
   firebase.auth().onAuthStateChanged((user) => {
     if (user) {
@@ -42,14 +78,102 @@ export default class newsApp extends Component {
   });
   }
 
+  componentDidMount() {
+    // iOS: show permission prompt for the first call. later just check permission in user settings
+       // Android: check permission in user settings
+       FCM.requestPermissions().then(()=>console.log('granted')).catch(()=>console.log('notification permission rejected'));
+
+       FCM.getFCMToken().then(token => {
+           console.log(token)
+           // store fcm token in your server
+       });
+
+       this.notificationListener = FCM.on(FCMEvent.Notification, async (notif) => {
+           // optional, do some component related stuff
+       });
+
+       // initial notification contains the notification that launchs the app. If user launchs app by clicking banner, the banner notification info will be here rather than through FCM.on event
+       // sometimes Android kills activity when app goes to background, and when resume it broadcasts notification before JS is run. You can use FCM.getInitialNotification() to capture those missed events.
+       // initial notification will be triggered all the time even when open app by icon so send some action identifier when you send notification
+       FCM.getInitialNotification().then(notif => {
+          console.log(notif)
+       });
+   }
+
+  componentWillUnmount() {
+    // stop listening for events
+        this.notificationListener.remove();
+    }
+
+  otherMethods(){
+
+    FCM.subscribeToTopic('/topics/foo-bar');
+    FCM.unsubscribeFromTopic('/topics/foo-bar');
+    FCM.presentLocalNotification({
+      id: 'UNIQ_ID_STRING',                               // (optional for instant notification)
+      title: 'My Notification Title',                     // as FCM payload
+      body: 'My Notification Message',                    // as FCM payload (required)
+      sound: 'default',                                   // as FCM payload
+      priority: 'high',                                   // as FCM payload
+      click_action: 'ACTION',                             // as FCM payload
+      badge: 10,                                          // as FCM payload IOS only, set 0 to clear badges
+      number: 10,                                         // Android only
+      ticker: 'My Notification Ticker',                   // Android only
+      auto_cancel: true,                                  // Android only (default true)
+      large_icon: 'ic_launcher',                           // Android only
+      icon: 'ic_launcher',                                // as FCM payload, you can relace this with custom icon you put in mipmap
+      big_text: 'Show when notification is expanded',     // Android only
+      sub_text: 'This is a subText',                      // Android only
+      color: 'red',                                       // Android only
+      vibrate: 300,                                       // Android only default: 300, no vibration if you pass 0
+      group: 'group',                                     // Android only
+      picture: 'https://google.png',                      // Android only bigPicture style
+      ongoing: true,                                      // Android only
+      my_custom_data:'my_custom_field_value',             // extra data you want to throw
+      lights: true,                                       // Android only, LED blinking (default false)
+      show_in_foreground                                  // notification when app is in foreground (local & remote)
+    });
+
+    FCM.scheduleLocalNotification({
+      fire_date: new Date().getTime(),      //RN's converter is used, accept epoch time and whatever that converter supports
+      id: 'UNIQ_ID_STRING',    //REQUIRED! this is what you use to lookup and delete notification. In android notification with same ID will override each other
+      body: 'from future past',
+      repeat_interval: 'week' //day, hour
+    });
+
+    FCM.getScheduledLocalNotifications().then(notif=>console.log(notif));
+
+    //these clears notification from notification center/tray
+    FCM.removeAllDeliveredNotifications();
+    FCM.removeDeliveredNotification('UNIQ_ID_STRING');
+
+    //these removes future local notifications
+    FCM.cancelAllLocalNotifications();
+    FCM.cancelLocalNotification('UNIQ_ID_STRING');
+
+    FCM.setBadgeNumber(1);                                       // iOS only and there's no way to set it in Android, yet.
+    FCM.getBadgeNumber().then(number => console.log(number));     // iOS only and there's no way to get it in Android, yet.
+    FCM.send('984XXXXXXXXX', {
+      my_custom_data_1: 'my_custom_field_value_1',
+      my_custom_data_2: 'my_custom_field_value_2'
+    });
+
+    FCM.deleteInstanceId()
+      .then(() => {
+      //Deleted instance id successfully
+      //This will reset Instance ID and revokes all tokens.
+      })
+      .catch(error => {
+      //Error while deleting instance id
+    });
+  }
+
   render() {
     switch (this.state.loggedIn) {
       case true:
         return (
-          <StyleProvider  style={getTheme(OffkeyTheme)}>
+          <StyleProvider style={getTheme(OffkeyTheme)}>
             <Container>
-              {/* <AppHeader/> */}
-              {/* <News/> */}
               <Router>
                 <Scene key="root">
                   <Scene key="news" component={News} title="News" hideNavBar={true}/>
@@ -57,7 +181,7 @@ export default class newsApp extends Component {
                   <Scene key="settings" component={Settings} title="Settings" hideNavBar={true}/>
                 </Scene>
               </Router>
-              <AppFooter/>
+              <AppFooter />
             </Container>
           </StyleProvider>
           );
--- ios/newsApp/AppDelegate.m
+++ ios/newsApp/AppDelegate.m
@@ -7,31 +7,61 @@
  * of patent rights can be found in the PATENTS file in the same directory.
  */
 
+@import Firebase;
 #import "AppDelegate.h"
 
 #import <React/RCTBundleURLProvider.h>
 #import <React/RCTRootView.h>
+#import "RNFIRMessaging.h"
 
 @implementation AppDelegate
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
-{
-  NSURL *jsCodeLocation;
+  {
+    NSURL *jsCodeLocation;
 
-  jsCodeLocation = [[RCTBundleURLProvider sharedSettings] jsBundleURLForBundleRoot:@"index" fallbackResource:nil];
+    jsCodeLocation = [[RCTBundleURLProvider sharedSettings] jsBundleURLForBundleRoot:@"index" fallbackResource:nil];
 
-  RCTRootView *rootView = [[RCTRootView alloc] initWithBundleURL:jsCodeLocation
-                                                      moduleName:@"newsApp"
-                                               initialProperties:nil
-                                                   launchOptions:launchOptions];
-  rootView.backgroundColor = [[UIColor alloc] initWithRed:1.0f green:1.0f blue:1.0f alpha:1];
+    RCTRootView *rootView = [[RCTRootView alloc] initWithBundleURL:jsCodeLocation
+                                                        moduleName:@"newsApp"
+                                                 initialProperties:nil
+                                                     launchOptions:launchOptions];
+    rootView.backgroundColor = [[UIColor alloc] initWithRed:1.0f green:1.0f blue:1.0f alpha:1];
 
-  self.window = [[UIWindow alloc] initWithFrame:[UIScreen mainScreen].bounds];
-  UIViewController *rootViewController = [UIViewController new];
-  rootViewController.view = rootView;
-  self.window.rootViewController = rootViewController;
-  [self.window makeKeyAndVisible];
-  return YES;
-}
+    self.window = [[UIWindow alloc] initWithFrame:[UIScreen mainScreen].bounds];
+    UIViewController *rootViewController = [UIViewController new];
+    rootViewController.view = rootView;
+    self.window.rootViewController = rootViewController;
+    [self.window makeKeyAndVisible];
+    [FIRApp configure];
+    [[UNUserNotificationCenter currentNotificationCenter] setDelegate:self];
+    
+    return YES;
+  }
+- (void)userNotificationCenter:(UNUserNotificationCenter *)center willPresentNotification:(UNNotification *)notification withCompletionHandler:(void (^)(UNNotificationPresentationOptions))completionHandler
+ {
+     [RNFIRMessaging willPresentNotification:notification withCompletionHandler:completionHandler];
+   }
+
+ #if defined(__IPHONE_11_0)
+ - (void)userNotificationCenter:(UNUserNotificationCenter *)center didReceiveNotificationResponse:(UNNotificationResponse *)response withCompletionHandler:(void (^)(void))completionHandler
+ {
+     [RNFIRMessaging didReceiveNotificationResponse:response withCompletionHandler:completionHandler];
+   }
+ #else
+ - (void)userNotificationCenter:(UNUserNotificationCenter *)center didReceiveNotificationResponse:(UNNotificationResponse *)response withCompletionHandler:(void(^)())completionHandler
+ {
+     [RNFIRMessaging didReceiveNotificationResponse:response withCompletionHandler:completionHandler];
+   }
+ #endif
+
+// //You can skip this method if you don't want to use local notification
+// -(void)application:(UIApplication *)application didReceiveLocalNotification:(UILocalNotification *)notification {
+//     [RNFIRMessaging didReceiveLocalNotification:notification];
+//   }
+//
+// - (void)application:(UIApplication *)application didReceiveRemoteNotification:(nonnull NSDictionary *)userInfo fetchCompletionHandler:(nonnull void (^)(UIBackgroundFetchResult))completionHandler{
+//     [RNFIRMessaging didReceiveRemoteNotification:userInfo fetchCompletionHandler:completionHandler];
+//   }
 
 @end
